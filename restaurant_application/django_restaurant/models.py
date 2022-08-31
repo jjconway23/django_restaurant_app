2b1023fdd381e68d2be0d547bc7249da6160769b
Jacob Peat <jacob.peat01@gmail.com>
2022-08-31 09:58:46 +0100

    Create purchase model

--- restaurant_application/django_restaurant/models.py
+++ restaurant_application/django_restaurant/models.py
@@ -16,3 +16,8 @@ class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem,on_delete=models.CASCADE)
     ingredients = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
     quantity = models.CharField(max_length=50, blank=False)
+
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem,on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
