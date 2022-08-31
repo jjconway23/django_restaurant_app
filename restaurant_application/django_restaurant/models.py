2ac7fa5f42a472d379232ba76d84a83b1f45e1dd
Jacob Peat <jacob.peat01@gmail.com>
2022-08-31 09:44:37 +0100

    Create the MenuItem model

--- restaurant_application/django_restaurant/models.py
+++ restaurant_application/django_restaurant/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Ingredients(models.Model):
-    name = models.CharField(max_length=50)
-    quantity = models.IntegerField()
-    unit = models.CharField(max_length=20)
-    price_per_unit = models.DecimalField(max_digits=6, decimal_places=2)
+    name = models.CharField(max_length=50, blank=False)
+    quantity = models.IntegerField(blank=False)
+    unit = models.CharField(max_length=20, blank=False)
+    price_per_unit = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
 
 
+class MenuItem(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
