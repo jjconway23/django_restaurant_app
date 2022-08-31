fd8c8a086964a1e88959f9d0a42bb028059078e6
Jacob Peat <jacob.peat01@gmail.com>
2022-08-31 10:04:25 +0100

    Register models

--- restaurant_application/django_restaurant/admin.py
+++ restaurant_application/django_restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from restaurant_application.django_restaurant.models import Ingredients, MenuItem, RecipeRequirement, Purchase
+
 # Register your models here.
+admin.site.register(Ingredients)
+admin.site.register(MenuItem)
+admin.site.register(RecipeRequirement)
+admin.site.register(Purchase)
