917e21ce0faf7fc208b3741358c89bca9ceeb600
Jacob Peat <jacob.peat01@gmail.com>
2022-08-31 09:41:51 +0100

    Create Ingredients model

--- restaurant_application/django_restaurant/models.py
+++ restaurant_application/django_restaurant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Ingredients(models.Model):
+    name = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=20)
+    price_per_unit = models.DecimalField(max_digits=6, decimal_places=2)
+
+
